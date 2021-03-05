fb42f9a43f526ddd0b34b6c3b90d3ec3f63ae10b
Yuri Yakymiv <ypyakymiv@gmail.com>
2021-03-05 12:50:27 -0500

    added main shell loop and utils

---
+++ ysh.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "ysh_utils.h"
+
+int main(int argc, char **argv) {
+  do {
+    write_prompt();
+  } while(0);
+}
---
+++ ysh_utils.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#define SHELL_PROMPT "ysh>"
+
+void write_prompt() {
+  printf(SHELL_PROMPT);  
+}
